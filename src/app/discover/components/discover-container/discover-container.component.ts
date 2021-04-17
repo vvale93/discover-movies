452f1b4635e4b0bbf812fb039eb77699b6d295fa
Valeria <vvale.93@hotmail.com>
2021-04-17 17:16:47 -0300

    Updated to scss

--- src/app/discover/components/discover-container/discover-container.component.ts
+++ src/app/discover/components/discover-container/discover-container.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-discover-container',
   templateUrl: './discover-container.component.html',
-  styleUrls: ['./discover-container.component.css']
+  styleUrls: ['./discover-container.component.scss']
 })
 export class DiscoverContainerComponent implements OnInit {
 
