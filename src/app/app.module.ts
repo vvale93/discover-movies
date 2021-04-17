c746aa8d371eb3927ca8a59efa8d9b19595c1ee7
Valeria <vvale.93@hotmail.com>
2021-04-17 16:46:01 -0300

    created discover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SpinnerComponent } from './spinner/component/spinner.component';
+import { DiscoverComponent } from './discover/components/discover/discover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    DiscoverComponent
   ],
   imports: [
     BrowserModule,
