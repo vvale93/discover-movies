a27f0e1f98fd87ab8e20871ebd28e32f94395fe8
Valeria <vvale.93@hotmail.com>
2021-04-17 18:14:11 -0300

    Added navbar and basic styles for main container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SpinnerComponent } from './spinner/component/spinner.component';
 import { DiscoverContainerComponent } from './discover/components/discover-container/discover-container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpinnerComponent,
-    DiscoverContainerComponent
+    DiscoverContainerComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, 
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
